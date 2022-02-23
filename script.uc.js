904309c73ec03cd799d8bbea2f760b092c7f28a5
Xinkai Chen <xinkai.chen@qq.com>
2022-02-23 21:51:43 +0800

    Improvements.
    1. Support maximized windows on Windows 10/11.
    2. Set sidebar header as window dragging area.
    3. Avoid duplicate window control buttons when menus are active.

--- script.uc.js
+++ script.uc.js
@@ -10,8 +10,32 @@
     }
 
     const SIDEBAR_ACTION = "treestyletab_piro_sakura_ne_jp-sidebar-action";
+    const TSTC_TABS_HIDE_FLAG = "tstc-tabs-hide";
+
+    const STYLE = `
+    :root[${TSTC_TABS_HIDE_FLAG}][tabsintitlebar] #titlebar {
+        appearance: none;
+    }
+
+    :root[${TSTC_TABS_HIDE_FLAG}][tabsintitlebar] #toolbar-menubar[autohide="true"] {
+        height: auto;
+    }
+
+    :root[${TSTC_TABS_HIDE_FLAG}][tabsintitlebar] #toolbar-menubar .titlebar-buttonbox-container {
+        display: none;
+    }
+
+    :root[${TSTC_TABS_HIDE_FLAG}][tabsintitlebar] #TabsToolbar {
+        display: none;
+    }
+
+    :root[${TSTC_TABS_HIDE_FLAG}] #sidebar-header {
+        -moz-window-dragging: drag;
+    }
+    `
 
     try {
+        const documentElement = document.documentElement;
         const tabsToolbar = document.getElementById("TabsToolbar");
         const navBar = document.getElementById("nav-bar");
 
@@ -21,29 +45,56 @@
 
         const sidebarBox = document.getElementById("sidebar-box");
 
+        let statusHide = -1;
+
         function hideOriginalTabs() {
             navBar.appendChild(accessibilityIndicator);
             navBar.appendChild(privateBrowsingIndicator);
             navBar.appendChild(titlebarButtonboxContainer);
-            tabsToolbar.style.display = "none";
+            documentElement.setAttribute(TSTC_TABS_HIDE_FLAG, "true");
+            statusHide = 1;
         }
 
         function undo() {
             tabsToolbar.appendChild(accessibilityIndicator);
             tabsToolbar.appendChild(privateBrowsingIndicator);
             tabsToolbar.appendChild(titlebarButtonboxContainer);
-            tabsToolbar.style.display = "";
+            documentElement.removeAttribute(TSTC_TABS_HIDE_FLAG);
+            statusHide = 0;
+        }
+
+        function compensate() {
+            if (statusHide === 1 && window.windowState === 1) {
+                const marginTopCompensate = -window.screenY;
+                documentElement.style["margin-top"] = `${marginTopCompensate}px`;
+                if (marginTopCompensate === 0) {
+                    documentElement.style["height"] = "100%";
+                } else {
+                    documentElement.style["height"] = `calc(100% - ${marginTopCompensate}px)`;
+                }
+            } else {
+                documentElement.style["height"] = "100%";
+                documentElement.style["margin-top"] = "0";
+            }
         }
 
         function refresh() {
-            const shouldHide = sidebarBox.attributes.sidebarcommand?.value === SIDEBAR_ACTION && sidebarBox.attributes.hidden?.value !== "true";
+            const shouldHide = sidebarBox.getAttribute("sidebarcommand") === SIDEBAR_ACTION && sidebarBox.getAttribute("hidden") !== "true";
             if (shouldHide) {
                 hideOriginalTabs();
             } else {
                 undo();
             }
+            compensate();
         }
 
+        window.addEventListener("resize", compensate);
+
+        {
+            const styleSheet = document.createElement("style");
+            styleSheet.innerText = STYLE;
+            document.head.appendChild(styleSheet)
+        }
         refresh();
 
         const observer = new MutationObserver((mutationsList, _observer) => {
@@ -63,3 +114,4 @@
         error(e);
     }
 })();
+
